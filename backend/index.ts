ba808664d9248b6226f4c12a1345e571a0f9870b
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2023-11-06 19:37:28 +0300

    api for farmer.get

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response , Application } from 'express';
 import { PrismaClient } from '@prisma/client';
 import UserRouter from "./routes/user.route";
+import FarmerRouter from "./routes/farmer.route";
 
  export const prisma = new PrismaClient();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 
 //Register API routes
 app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/farmer ", FarmerRouter);
 
   // Catch unregistered routes
   app.all("*", (req: Request, res: Response) => {
@@ -41,72 +43,3 @@ main()
     process.exit(1)
   });
   
-// import express, { Express, Request, Response , Application } from 'express';
-// import { PrismaClient } from '@prisma/client';
-// export const prisma = new PrismaClient();
-// const app = express();
-
-// app.use(express.json());
-
-// // Define routes for CRUD operations
-
-// // Create a new User
-// app.post('/user', async (req: Request, res: Response) => {
-//   try {
-//     const user = await prisma.user.create({
-//       data: req.body,
-//     });
-//     res.json(user);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'An error occurred' });
-//   }
-// });
-
-// // Retrieve all Users
-// app.get('/users', async (req:Request, res:Response) => {
-//   const users = await prisma.user.findMany();
-//   res.json(users);
-// });
-
-// // Retrieve a User by ID
-// app.get('/users/:id', async (req:Request, res:Response) => {
-//   const { id } = req.params;
-//   const user = await prisma.user.findUnique({
-//     where: { id: parseInt(id) },
-//   });
-//   if (user) {
-//     res.json(user);
-//   } else {
-//     res.status(404).json({ error: 'User not found' });
-//   }
-// });
-
-// // Update a User by ID
-// app.put('/users/:id', async (req:Request, res:Response) => {
-//   const { id } = req.params;
-//   try {
-//     const updatedUser = await prisma.user.update({
-//       where: { id: parseInt(id) },
-//       data: req.body,
-//     });
-//     res.json(updatedUser);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'An error occurred' });
-//   }
-// });
-
-// // Delete a User by ID
-// app.delete('/users/:id', async (req:Request, res:Response) => {
-//   const { id } = req.params;
-//   const deletedUser = await prisma.user.delete({
-//     where: { id: parseInt(id) },
-//   });
-//   res.json(deletedUser);
-// });
-
-// // Start the server
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// })
